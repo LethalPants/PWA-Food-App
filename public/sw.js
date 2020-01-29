7120af382fe3839df3bcecf529d9f986499bcac2
LethalPants <48921721+LethalPants@users.noreply.github.com>
2020-01-29 17:57:06 +0530

    Deploy to firebase

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = "site-static-v4";
-const dynamicCacheName = "site-dynamic-v7";
+const staticCacheName = "site-static-v6";
+const dynamicCacheName = "site-dynamic-v9";
 
 const assets = [
   "/",
